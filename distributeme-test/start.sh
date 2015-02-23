ab23e563c65f94b0862285969372862f6f07cf57
Leon Rosenberg <leon.rosenberg@anotheria.net>
2015-02-23 17:42:06 +0100

    fixing the pom and version infos

--- distributeme-test/start.sh
+++ distributeme-test/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export VERSION=2.1.4-SNAPSHOT
+export VERSION=2.2.1-SNAPSHOT
 
-CLASSPATH=test/appdata:target/distributeme-test-$VERSION-jar-with-dependencies.jar
+CLASSPATH=src/test/resources:target/distributeme-test-$VERSION-jar-with-dependencies.jar
 echo CLASSPATH: $CLASSPATH
 java -Xmx256M -Xms64M -classpath $CLASSPATH -Dconfigureme.defaultEnvironment=test $*
