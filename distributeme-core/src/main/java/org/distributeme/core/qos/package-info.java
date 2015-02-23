a2f8ad0d13b69c66a3c4549098a67cd1c3e8fe6e
Leon Rosenberg <leon.rosenberg@anotheria.net>
2015-02-23 17:46:38 +0100

    javadoc

--- distributeme-core/src/main/java/org/distributeme/core/qos/package-info.java
+++ distributeme-core/src/main/java/org/distributeme/core/qos/package-info.java
@@ -1,6 +1,6 @@
 /**
  * This package contains quality of service utilities for DistributeMe.
- * To enable it you will have to enable the quality of service interceptor.
+ * To enable it you will have to enable the quality of service interceptor (org.distributeme.core.qos.QOSMonitoringInterceptor).
  * @author lrosenberg
  * @since 20.02.15 17:28
  */
