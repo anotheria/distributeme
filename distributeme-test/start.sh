06f2bc93a3137ce4c536a8ccc15604305e8770c1
Leon Rosenberg <leon.rosenberg@anotheria.net>
2016-01-25 08:54:56 +0100

    adjusted start script to use 2.2.4-SNAPSHOT

--- distributeme-test/start.sh
+++ distributeme-test/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export VERSION=2.2.2-SNAPSHOT
+export VERSION=2.2.4-SNAPSHOT
 
 CLASSPATH=src/test/resources:target/distributeme-test-$VERSION-jar-with-dependencies.jar
 echo CLASSPATH: $CLASSPATH
