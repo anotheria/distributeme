f489f0ae174c227d6d7ae1803cecd6d7d4745258
Ronald Böhling <ronald.boehling@parship.com>
2017-02-28 16:10:23 +0100

    DISTRIBUTEME-39. Introduce ConsulRegistryConnector

---
+++ distributeme-consul-registry-connector/src/main/java/org/distributeme/consulintegration/ConsulRegistryConnector.java
@@ -0,0 +1,234 @@
+package org.distributeme.consulintegration;
+
+import java.util.List;
+import java.util.StringTokenizer;
+
+import com.google.gson.Gson;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import net.anotheria.util.StringUtils;
+import org.distributeme.core.Location;
+import org.distributeme.core.RegistryConnector;
+import org.distributeme.core.ServiceDescriptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static org.distributeme.core.ServiceDescriptor.HOST_SEPARATOR;
+import static org.distributeme.core.ServiceDescriptor.INSTANCE_SEPARATOR;
+import static org.distributeme.core.ServiceDescriptor.PORT_SEPARATOR;
+import static org.distributeme.core.ServiceDescriptor.TIMESTAMP_SEPARATOR;
+
+
+/**
+ * Created by rboehling on 2/28/17.
+ */
+public class ConsulRegistryConnector implements RegistryConnector {
+
+	private Logger logger = LoggerFactory.getLogger(ConsulRegistryConnector.class);
+
+	private static final String HOST = "http://psdeoffws0077.parship.internal:8500";
+	private static final String TAG_INSTANCE_ID = "instanceId";
+	private static final String TAG_PROTOCOL = "protocol";
+	private static final String TAG_TIMESTAMP = "timestamp";
+
+	static class ConsulServiceDescription {
+		String ID;
+		String Name;
+		String[] Tags = new String[4];
+		String Address = "192.168.140.63";
+		int Port;
+		boolean EnableTagOverride = false;
+
+		void addJmxPort(String jmxPort) {
+			Tags[0] = "jmx=" + jmxPort;
+		}
+
+		void addInstanceId(String instanceId) {
+			Tags[1] = TAG_INSTANCE_ID + "=" + instanceId;
+		}
+
+		void addProtocol(String protocol) {
+			Tags[2] = TAG_PROTOCOL + "=" + protocol;
+		}
+
+		void addTimestamp(long timestamp) {
+			Tags[3] = TAG_TIMESTAMP + "=" + timestamp;
+		}
+	}
+
+	static class ServiceResolveReply{
+
+
+		List<String> ServiceTags;
+		String ServiceAddress;
+		int ServicePort;
+		String ServiceID;
+
+		public List<String> getServiceTags() {
+			return ServiceTags;
+		}
+
+		public String getServiceAddress() {
+			return ServiceAddress;
+		}
+
+		public int getServicePort() {
+			return ServicePort;
+		}
+
+		@Override
+		public String toString() {
+			return "ServiceResolveReply{" +
+					"ServiceTags=" + ServiceTags +
+					", ServiceAddress='" + ServiceAddress + '\'' +
+					", ServicePort=" + ServicePort +
+					'}';
+		}
+
+		public String getServiceID() {
+			return ServiceID;
+		}
+
+		public String getInstanceId() {
+			return getTag(TAG_INSTANCE_ID);
+		}
+
+		private String getTag(String tag) {
+			for(String t: getServiceTags()) {
+				String[] tokens = StringUtils.tokenize(t, '=');
+				if(tokens[0].equals(tag)) {
+					return tokens[1];
+				}
+			}
+			throw new IllegalArgumentException("Tag " + tag + " was not found for " + getServiceID());
+		}
+
+		public String getProtocol() {
+			return getTag(TAG_PROTOCOL);
+		}
+
+		public long getTimestamp() {
+			return Long.parseLong(getTag(TAG_TIMESTAMP));
+		}
+	}
+
+	@Override
+	public boolean bind(ServiceDescriptor service) {
+		ClientResponse response = null;
+		try {
+			Gson gson = new Gson();
+			ConsulServiceDescription serviceDescription = new ConsulServiceDescription();
+			serviceDescription.ID = service.getServiceId();
+			serviceDescription.Name = service.getServiceId();
+			serviceDescription.Port = service.getPort();
+			serviceDescription.addInstanceId(service.getInstanceId());
+			serviceDescription.addJmxPort("XXXXX");
+			serviceDescription.addProtocol(service.getProtocol());
+			serviceDescription.addTimestamp(service.getTimestamp());
+
+			String requestAsJsonString = gson.toJson(serviceDescription);
+
+			WebResource webResource = Client.create()
+											.resource(HOST + "/v1/agent/service/register");
+
+			response = webResource.accept("application/json")
+												 .put(ClientResponse.class, requestAsJsonString);
+
+
+
+			if (response.getStatus() != 200) {
+				logger.error("Registry returns status: " + response.getStatus());
+				return false;
+			}
+
+			return true;
+		} catch (RuntimeException e) {
+			logger.error("Could not bind service " + service);
+			return false;
+		} finally {
+			closeResponseNullSafe(response);
+		}
+	}
+
+	@Override
+	public boolean notifyBind(Location location, ServiceDescriptor descriptor) {
+		return false;
+	}
+
+	@Override
+	public boolean notifyUnbind(Location location, ServiceDescriptor descriptor) {
+		return false;
+	}
+
+	@Override
+	public boolean unbind(ServiceDescriptor service) {
+		ClientResponse response = null;
+		try {
+			WebResource webResource = Client.create().resource(HOST + "/v1/agent/service/deregister/" + service.getServiceId());
+
+			response = webResource.accept("application/json").get(ClientResponse.class);
+
+			if (response.getStatus() != 200) {
+				logger.error("Registry returns status: " + response.getStatus());
+				return false;
+			}
+			return true;
+		} catch (Exception e) {
+			logger.error("Could not unbind service " + service, e);
+			return false;
+		} finally {
+			closeResponseNullSafe(response);
+		}
+	}
+
+	@Override
+	public ServiceDescriptor resolve(ServiceDescriptor toResolve, Location loc) {
+		ClientResponse response = null;
+		try {
+			Gson gson = new Gson();
+			WebResource webResource = Client.create().resource(HOST + "/v1/catalog/service/" + toResolve.getServiceId());
+
+			response = webResource.accept("application/json").get(ClientResponse.class);
+
+			String output = response.getEntity(String.class);
+			ServiceResolveReply[] serviceResolveReplies = gson.fromJson(output, ServiceResolveReply[].class);
+			if(serviceResolveReplies.length == 0) {
+				return null;
+			}
+			ServiceResolveReply serviceResolveReply = serviceResolveReplies[0];
+
+			if (response.getStatus() != 200) {
+				logger.error("Failed : HTTP error code : " + response.getStatus());
+			}
+			return fromRegistrationString(serviceResolveReply);
+		} catch (Exception e) {
+			logger.error("Could not resolve ServiceDescriptor: ", e);
+		} finally {
+			closeResponseNullSafe(response);
+		}
+		return null;
+	}
+
+	private void closeResponseNullSafe(ClientResponse response) {
+		if(response != null) {
+			response.close();
+		}
+	}
+
+	/**
+	 * Factory method to create a service descriptor from a registration string (used for bind in the registry).
+	 *
+	 * @param serviceResolveReply a {@link String} object.
+	 * @return a {@link org.distributeme.core.ServiceDescriptor} object.
+	 */
+	private ServiceDescriptor fromRegistrationString(ServiceResolveReply serviceResolveReply){
+		int port = serviceResolveReply.getServicePort();
+		String host = serviceResolveReply.getServiceAddress();
+		long timestamp = serviceResolveReply.getTimestamp();
+		String protocol = serviceResolveReply.getProtocol();
+		String serviceId = serviceResolveReply.getServiceID();
+		String instanceId = serviceResolveReply.getInstanceId();
+		return new ServiceDescriptor(ServiceDescriptor.Protocol.valueOf(protocol.toUpperCase()), serviceId, instanceId, host, port, timestamp);
+	}
+}
