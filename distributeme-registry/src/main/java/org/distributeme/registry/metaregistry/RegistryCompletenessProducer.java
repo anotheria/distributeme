c2bec380260f36f1d18d311dd418d1552e8e1c8c
Leon Rosenberg <leon.rosenberg@anotheria.net>
2018-06-20 16:42:03 +0200

    upgraded moskito version

---
+++ distributeme-registry/src/main/java/org/distributeme/registry/metaregistry/RegistryCompletenessProducer.java
@@ -0,0 +1,10 @@
+package org.distributeme.registry.metaregistry;
+
+/**
+ * TODO comment this class
+ *
+ * @author lrosenberg
+ * @since 26.02.18 18:18
+ */
+public class RegistryCompletenessProducer {
+}
