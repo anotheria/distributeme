ed5d270c75e27164422650139a8ca3ced0190639
dvayanu <leon@anotheria.net>
2022-07-10 23:57:17 +0200

    adjust to new syntax

--- distributeme-registry/src/main/java/org/distributeme/registry/metaregistry/RegistryCompletenessProducer.java
+++ distributeme-registry/src/main/java/org/distributeme/registry/metaregistry/RegistryCompletenessProducer.java
@@ -5,12 +5,11 @@ import net.anotheria.moskito.core.counter.CounterStatsFactory;
 import net.anotheria.moskito.core.producers.IStatsProducer;
 import net.anotheria.moskito.core.registry.ProducerRegistryFactory;
 import net.anotheria.moskito.core.stats.TimeUnit;
-import net.anotheria.moskito.core.threshold.CustomThresholdProvider;
-import net.anotheria.moskito.core.threshold.ThresholdDefinition;
-import net.anotheria.moskito.core.threshold.ThresholdRepository;
-import net.anotheria.moskito.core.threshold.ThresholdStatus;
+import net.anotheria.moskito.core.threshold.*;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -27,37 +26,31 @@ public class RegistryCompletenessProducer implements CustomThresholdProvider, IS
 	private CounterStats stats ;
 	private List<CounterStats> statsList = new ArrayList<>(1);
 
+	private static String THRESHOLDS_NAMES[] = {"RegistryCompleteness"};
+
 	RegistryCompletenessProducer(){
 		config = MetaRegistryConfig.create();
 		stats = CounterStatsFactory.DEFAULT_INSTANCE.createStatsObject("Services");
 		statsList.add(stats);
 		ProducerRegistryFactory.getProducerRegistryInstance().registerProducer(this);
-		if (config.isMonitorRequiredServiceAmount()){
-			ThresholdDefinition definition = new ThresholdDefinition();
-			definition.setDescription("RegistryCompleteness");
-			definition.setIntervalName("default");
-			definition.setName("RegistryCompleteness");
-			definition.setProducerName(getProducerId());
-			definition.setStatName("counter");
-			definition.setTimeUnit(TimeUnit.MILLISECONDS); //this doesn't matter.
-			ThresholdRepository.getInstance().createThreshold(definition);
-		}
 	}
 
 	@Override
-	public ThresholdStatus getStatus() {
+	public List<String> getCustomThresholdNames() {
+		return Arrays.asList(THRESHOLDS_NAMES);
+	}
 
+	@Override
+	public CustomThresholdStatus getCustomThresholdStatus(String thresholdName) {
 		if (!config.isMonitorRequiredServiceAmount())
-			return ThresholdStatus.OFF;
+			return new CustomThresholdStatus(ThresholdStatus.OFF, "OFF");
 
 		return currentProducerCount.get() == config.getRequiredServiceAmount() ?
-				ThresholdStatus.GREEN : ThresholdStatus.RED;
+				new CustomThresholdStatus(ThresholdStatus.GREEN, ""+currentProducerCount.get()) :
+				new CustomThresholdStatus(ThresholdStatus.RED, "Expected "+config.getRequiredServiceAmount()
+				+", has "+currentProducerCount.get());
 	}
 
-	@Override
-	public String getCurrentValue() {
-		return "Services "+currentProducerCount.get();
-	}
 
 	public void setServiceCount(int count) {
 		currentProducerCount.set(count);
