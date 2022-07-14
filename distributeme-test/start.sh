d6da0c04da41ca1f029ca99dcd574996f610468c
dvayanu <leon@anotheria.net>
2022-07-14 22:57:12 +0200

    set test on ignore as it doesn't run on arm64 (m1) arch

--- distributeme-test/start.sh
+++ distributeme-test/start.sh
@@ -3,4 +3,4 @@ export VERSION=2.3.9-SNAPSHOT
 
 CLASSPATH=src/test/resources:target/distributeme-test-$VERSION-jar-with-dependencies.jar
 echo CLASSPATH: $CLASSPATH
-java -Xmx256M -Xms64M -classpath $CLASSPATH -Dconfigureme.defaultEnvironment=test $*
+java -Xmx256M -Xms64M -classpath $CLASSPATH -Djava.rmi.server.logCalls -Dconfigureme.defaultEnvironment=test $*
